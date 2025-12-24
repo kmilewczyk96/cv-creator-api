4fde8e2d1b76715fb5035b821b01f9a81a08b7df
kmilewczyk96 <kmilew0802@gmail.com>
2025-12-24 10:25:48 +0100

    Verification service updated.

--- app/services/verification_service.py
+++ app/services/verification_service.py
@@ -5,14 +5,13 @@ import resend
 from random import choice
 from string import Template, ascii_uppercase, digits
 
+from api.schemas.user import UserCreateResponse
+
 
 class VerificationService:
+    app_name = os.environ.get('APP_NAME')
     domain = os.environ.get('DOMAIN_NAME')
     resend.api_key = os.environ.get('RESEND_API_KEY')
-
-    def check(self):
-        print(self.domain)
-        print(resend.api_key)
 
     @staticmethod
     def generate_verification_code(length=6) -> str:
@@ -20,19 +19,19 @@ class VerificationService:
         characters = ascii_uppercase + digits
         return ''.join(choice(characters) for _ in range(length))
 
-    def send_verification_code(self, recipient: str):
+    def send_verification_code(self, recipient: UserCreateResponse):
         with open('/app/app/utils/email_templates/email_verification.html', 'r', encoding='utf-8') as template:
             src = Template(template.read())
-            html_content = src.substitute(
-                app_name='CV Creator',
-                full_name='Karol Milewczyk',
-                verification_code=self.generate_verification_code()
-            )
 
+        html_content = src.substitute(
+            app_name=self.app_name,
+            full_name=recipient.full_name,
+            verification_code=self.generate_verification_code()
+        )
         params: resend.Emails.SendParams = {
             'from': f'noreply@{self.domain}',
-            'to': recipient,
-            'subject': 'Welcome to CV Creator',
+            'to': recipient.email,
+            'subject': f'Welcome to {self.app_name}',
             'html': html_content,
 
         }
